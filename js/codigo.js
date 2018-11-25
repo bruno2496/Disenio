c455d01e27506a4874f9a7157af0bddab0442222
bruno2496 <45240176+bruno2496@users.noreply.github.com>
2018-11-25 01:43:50 -0300

    Add files via upload

--- js/codigo.js
+++ js/codigo.js
@@ -3,3 +3,23 @@ function inicio()
 {
     $("#popover").popover();
 }
+
+
+$(".jv_scrollup").hide();
+	
+	// fade in .jv_scrollup
+	$(function () {
+		$(window).scroll(function () {
+			if ($(this).scrollTop() > 100) {
+				$('.jv_scrollup').fadeIn();
+			} else {
+				$('.jv_scrollup').fadeOut();
+			}
+		});
+		// scroll body to 0px on click
+		$('.jv_scrollup').click(function () {
+			$('body,html').animate({
+				scrollTop: 0
+			}, 800);
+			return false;
+		});
